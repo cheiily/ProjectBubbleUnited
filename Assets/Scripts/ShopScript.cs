01f6064845a76d196cba4d39c5f449db9acd5d99
3Kudo <116746067+3Kudo@users.noreply.github.com>
2025-01-25 20:35:17 +0100

    shop

---
+++ Assets/Scripts/ShopScript.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopScript : MonoBehaviour
+{
+    public GameObject[] radars, turets, houses, greenHouses;
+    public GameObject sphere;
+    public int money;
+    [SerializeField]
+    private float waitTime;
+    private float timer;
+    private int zarabianing;
+    public TMP_Text moneyDisplay;
+
+
+    private void Start()
+    {
+        timer = 0;
+        zarabianing = 1;
+        money = 0;
+        moneyDisplay.text = money.ToString();
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > waitTime)
+        {
+            timer = 0;
+            money += zarabianing;
+            moneyDisplay.text = money.ToString();
+        }
+    }
+
+
+    public void BuyHouse()
+    {
+        if (money >= 5)
+        {
+            foreach (GameObject house in houses)
+            {
+                if (!house.active)
+                {
+                    money -= 5;
+                    zarabianing++;
+                    house.active = true;
+                    moneyDisplay.text = money.ToString();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void BuyGreenhous()
+    {
+        if (money >= 5)
+        {
+            foreach (GameObject greenHouse in greenHouses)
+            {
+                if (!greenHouse.active)
+                {
+                    money -= 5;
+                    greenHouse.active = true;
+                    moneyDisplay.text = money.ToString();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void BuyTuret()
+    {
+        if (money >= 5)
+        {
+            foreach (GameObject turetr in turets)
+            {
+                if (!turetr.active)
+                {
+                    money -= 5;
+                    turetr.active = true;
+                    moneyDisplay.text = money.ToString();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void BuyRadar()
+    {
+        if (money >= 5)
+        {
+            foreach (GameObject radar in radars)
+            {
+                if (!radar.active)
+                {
+                    money -= 5;
+                    radar.active = true;
+                    moneyDisplay.text = money.ToString();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void LoseHouse()
+    {
+        zarabianing--;
+    }
+
+    public void LoseGreenhous()
+    { 
+    }
+}
