8a60d29f3990dd8bdc4cc21e4faefb8f5e1bae47
3Kudo <116746067+3Kudo@users.noreply.github.com>
2025-01-26 13:01:30 +0100

    aj

--- Assets/Scripts/MenuInteractions.cs
+++ Assets/Scripts/MenuInteractions.cs
@@ -6,12 +6,11 @@ using UnityEngine.UI;
 public class MenuInteractions : MonoBehaviour
 {
     public VicinityActivation shopVicinity;
-    public GameObject shopPanel, napis;
+    public GameObject shopPanel;
     public Button defaultShopButton;
 
     public void OnSouth(InputValue ctx) {
         if (ctx.isPressed && shopVicinity.isWithin && !shopPanel.active) {
-            napis.SetActive(false);
             Time.timeScale = 0;
             shopPanel.SetActive(true);
             EventSystem.current.SetSelectedGameObject(defaultShopButton.gameObject);
@@ -21,7 +20,6 @@ public class MenuInteractions : MonoBehaviour
 
     public void OnEast(InputValue ctx) {
         if (ctx.isPressed) {
-            napis.SetActive(true);
             Time.timeScale = 1;
             shopPanel.SetActive(false);
             EventSystem.current.SetSelectedGameObject(null);
